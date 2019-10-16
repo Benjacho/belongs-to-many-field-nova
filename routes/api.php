08dd2376851abfbf7ed0d5feb5fabc3a4733fdb4
Tim Cifuentes Vargas <tim@haus.se>
2019-10-16 13:34:13 +0200

    Load options with ajax by default.

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+Route::get('/test', function() {
+   return 'hejhej';
+});
+
+Route::get('/{resource}/options/{relationship}', function(NovaRequest $request, $parent, $relationship) {
+    $resourceClass = $request->newResource();
+
+    $field = $resourceClass
+        ->availableFields($request)
+        ->where('component', 'BelongsToManyField')
+        ->where('attribute', $relationship)
+        ->first();
+
+    $query = $field->resourceClass::newModel();
+
+    return $field->resourceClass::relatableQuery($request, $query)->get()
+        ->mapInto($field->resourceClass)
+        ->filter(function ($resource) use ($request, $field) {
+            return $request->newResource()->authorizedToAttach($request, $resource->resource);
+        })->map(function($resource) {
+            return [
+                'id' => $resource->id,
+                'name' => $resource->title(),
+                'value' => $resource->getKey(),
+            ];
+        })->sortBy('name')
+        ->values();
+});
