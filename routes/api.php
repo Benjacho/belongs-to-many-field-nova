6c0084f4abe1896fd9a47d44231cb8dccb2c27a1
aBartn <bart.noordsij@gmail.com>
2019-11-29 13:18:48 +0100

    Update api.php
    move closure to controller

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,5 @@
 <?php
 
-use Laravel\Nova\Http\Requests\NovaRequest;
+use Benjacho\BelongsToManyField\Http\Controllers\ResourceController;
 
-Route::get('/{resource}/options/{relationship}/{optionsLabel}', function (NovaRequest $request, $parent, $relationship, $optionsLabel) {
-    $resourceClass = $request->newResource();
-
-    $field = $resourceClass
-        ->availableFields($request)
-        ->where('component', 'BelongsToManyField')
-        ->where('attribute', $relationship)
-        ->first();
-
-    $query = $field->resourceClass::newModel();
-
-    return $field->resourceClass::relatableQuery($request, $query)->get()
-        ->mapInto($field->resourceClass)
-        ->filter(function ($resource) use ($request, $field) {
-            return $request->newResource()->authorizedToAttach($request, $resource->resource);
-        })->map(function ($resource) use ($optionsLabel) {
-            return [
-                'id' => $resource->id,
-                $optionsLabel => $resource->title(),
-                'value' => $resource->getKey(),
-            ];
-        })->sortBy('name')
-        ->values();
-});
+Route::get('/{resource}/options/{relationship}/{optionsLabel}', [ResourceController::class, 'index']);
