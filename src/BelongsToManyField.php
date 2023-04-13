0388cf7f107f76b8c4312443fc2ce4c1d334373c
James William Dumay <james@hashgifted.com>
2023-04-13 13:23:56 +1000

    Fixes typing error on jsonSerialize() on Nova 4
    Fixes PHP Fatal error:  Declaration of Benjacho\BelongsToManyField\BelongsToManyField::jsonSerialize() must be compatible with Laravel\Nova\Fields\Field::jsonSerialize(): array

--- src/BelongsToManyField.php
+++ src/BelongsToManyField.php
@@ -180,7 +180,7 @@ class BelongsToManyField extends Field
         }
     }
 
-    public function jsonSerialize() : array
+    public function jsonSerialize(): array
     {
         $this->resolveOptions();
 
@@ -204,7 +204,6 @@ class BelongsToManyField extends Field
             'textAlign' => $this->textAlign,
             'value' => $this->value,
             'viewable' => $this->viewable,
-            'visible' => $this->visible,
             'validationKey' => $this->validationKey(),
         ], $this->meta());
     }
